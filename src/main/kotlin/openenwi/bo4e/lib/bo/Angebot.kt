d0ef3d7770aafd53fbcbdfec64887e2c642f5357
Florian Kubis <mail@kubisflo.de>
2021-07-15 12:00:46 +0200

    Add BOs Angebot, Ansprechpartner, Ausschreibung
    closes #3

---
+++ src/main/kotlin/openenwi/bo4e/lib/bo/Angebot.kt
@@ -0,0 +1,30 @@
+package openenwi.bo4e.lib.bo
+
+import openenwi.bo4e.lib.com.Angebotsvariante
+import openenwi.bo4e.lib.enum.BOTyp
+import openenwi.bo4e.lib.enum.Sparte
+import java.time.ZonedDateTime
+
+/**
+ * Mit diesem BO kann ein Versorgungsangebot zur Strom- oder Gasversorgung
+ * oder die Teilnahme an einer Ausschreibung übertragen werden.
+ * Es können verschiedene Varianten enthalten sein (z.B. ein- und mehrjährige Laufzeit).
+ * Innerhalb jeder Variante können Teile enthalten sein,
+ * die jeweils für eine oder mehrere Marktlokationen erstellt werden.
+ *
+ * veröffentlicht am 17. Januar 2018
+ *
+ * @see "https://www.bo4e.de/dokumentation/geschaeftsobjekte/bo-angebot"
+ */
+data class Angebot(
+    val angebotsnummer: String,
+    val anfragereferenz: String?,
+    val angebotsdatum: ZonedDateTime,
+    val sparte: Sparte,
+    val bindefrist: ZonedDateTime?,
+    val angebotsgeber: Geschaeftspartner,
+    val angebotsnehmer: Geschaeftspartner,
+    val unterzeichnerAngebotsnehmer: Ansprechpartner?,
+    val unterzeichnerAngebotsgeber: Ansprechpartner?,
+    val varianten: Set<Angebotsvariante>
+): Geschaeftsobjekt(boTyp = BOTyp.ANGEBOT)
---
+++ src/main/kotlin/openenwi/bo4e/lib/bo/Ansprechpartner.kt
@@ -0,0 +1,29 @@
+package openenwi.bo4e.lib.bo
+
+import openenwi.bo4e.lib.com.Adresse
+import openenwi.bo4e.lib.com.Rufnummer
+import openenwi.bo4e.lib.com.Zustaendigkeit
+import openenwi.bo4e.lib.enum.Anrede
+import openenwi.bo4e.lib.enum.BOTyp
+import openenwi.bo4e.lib.enum.Titel
+
+/**
+ * Modellierung eines Ansprechpartners für einen bestimmten Geschäftspartner.
+ *
+ * veröffentlicht am 17. Januar 2018
+ *
+ * @see "https://www.bo4e.de/dokumentation/geschaeftsobjekte/bo-ansprechpartner"
+ */
+data class Ansprechpartner(
+    val anrede: Anrede?,
+    val individuelleAnrede: String?,
+    val titel: Titel?,
+    val vorname: String?,
+    val nachname: String,
+    val eMailAdresse: String?,
+    val kommentar: String?,
+    val geschaeftspartner: Geschaeftspartner,
+    val adresse: Adresse?,
+    val rufnummer: Rufnummer?,
+    val zustaendigkeit: Zustaendigkeit?
+): Geschaeftsobjekt(boTyp = BOTyp.ANSPRECHPARTNER)
