355c246958c8adb99994549cde6e89cc476b8c97
Florian Kubis <mail@kubisflo.de>
2021-07-15 09:06:41 +0200

    Add COMs Unterschrift, Verbrauch, Vertragsteil, Zaehlwerk
    refs #2

---
+++ src/main/kotlin/openenwi/bo4e/lib/com/Verbrauch.kt
@@ -0,0 +1,22 @@
+package openenwi.bo4e.lib.com
+
+import openenwi.bo4e.lib.enum.Mengeneinheit
+import openenwi.bo4e.lib.enum.Wertermittlungsverfahren
+import java.math.BigDecimal
+import java.time.ZonedDateTime
+
+/**
+ * Abbildung eines zeitlich abgegrenzten Verbrauchs.
+ *
+ * veröffentlicht am 17. Januar 2018
+ *
+ * @see "https://www.bo4e.de/dokumentation/komponenten/com-verbrauch"
+ */
+data class Verbrauch(
+    val startdatum: ZonedDateTime,
+    val enddatum: ZonedDateTime,
+    val wertermittlungsverfahren: Wertermittlungsverfahren,
+    val obiskennzahl: String,
+    val wert: BigDecimal,
+    val einheit: Mengeneinheit
+)
---
+++ src/main/kotlin/openenwi/bo4e/lib/com/Vertragsteil.kt
@@ -0,0 +1,21 @@
+package openenwi.bo4e.lib.com
+
+import java.time.ZonedDateTime
+
+/**
+ * Abbildung für einen Vertragsteil.
+ * Der Vertragsteil wird dazu verwendet,
+ * eine vertragliche Leistung in Bezug zu einer Lokation (Markt- oder Messlokation) festzulegen.
+ *
+ * veröffentlicht am 05. September 2018
+ *
+ * @see "https://www.bo4e.de/dokumentation/komponenten/com-vertragsteil"
+ */
+data class Vertragsteil(
+    val vertragsteilbeginn: ZonedDateTime,
+    val vertragsteilende: ZonedDateTime,
+    val lokation: String?,
+    val vertraglichFixierteMenge: Menge?,
+    val minimaleAbnahmemenge: Menge?,
+    val maximaleAbnahmemenge: Menge?
+)
