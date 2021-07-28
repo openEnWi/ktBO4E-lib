8cd7dd882f706c96a836fb8b4ae046443773afd5
Florian Kubis <mail@kubisflo.de>
2021-07-28 09:16:01 +0200

    Add BOs Messlokation, Zaehler
    refs #3

---
+++ src/main/kotlin/openenwi/bo4e/lib/bo/Messlokation.kt
@@ -0,0 +1,29 @@
+package openenwi.bo4e.lib.bo
+
+import openenwi.bo4e.lib.com.*
+import openenwi.bo4e.lib.enum.BOTyp
+import openenwi.bo4e.lib.enum.Netzebene
+import openenwi.bo4e.lib.enum.Sparte
+
+/**
+ * Objekt zur Aufnahme der Informationen zu einer Messlokation.
+ *
+ * veröffentlicht am 17. Januar 2018
+ *
+ * @see "https://www.bo4e.de/dokumentation/geschaeftsobjekte/bo-messlokation"
+ */
+data class Messlokation(
+    val messlokationsId: String,
+    val sparte: Sparte,
+    val netzebeneMessung: Netzebene?,
+    val messgebietnr: String?,
+    val grundzustaendigerMSBCodenr: String?,
+    val grundzustaendigerMSBIMCodenr: String?,
+    val grundzustaendigerMDLCodenr: String?,
+    val messlokationszaehler: Set<Zaehler>,
+    val messadresse: Adresse?,
+    val geoadresse: Geokoordinaten?,
+    val katasterinformation: Katasteradresse?,
+    val geraete: Set<Hardware>,
+    val messdienstleistung: Set<Dienstleistung>
+    ): Geschaeftsobjekt(boTyp = BOTyp.MESSLOKATION)
---
+++ src/main/kotlin/openenwi/bo4e/lib/bo/Zaehler.kt
@@ -0,0 +1,26 @@
+package openenwi.bo4e.lib.bo
+
+import openenwi.bo4e.lib.com.Zaehlwerk
+import openenwi.bo4e.lib.enum.*
+import java.math.BigDecimal
+import java.time.ZonedDateTime
+
+/**
+ * Mit diesem Geschäftsobjekt wird die Information zu einem Zähler abgebildet.
+ *
+ * veröffentlicht am 17. Januar 2018
+ *
+ * @see "https://www.bo4e.de/dokumentation/geschaeftsobjekte/bo-zahler"
+ */
+data class Zaehler(
+    val zaehlernummer: String,
+    val sparte: Sparte,
+    val zaehlerauspraegung: Zaehlerauspraegung,
+    val zaehlertyp: Zaehlertyp,
+    val tarifart: Tarifart,
+    val zaehlerkonstante: BigDecimal?,
+    val eichungBis: ZonedDateTime?,
+    val letzteEichung: ZonedDateTime?,
+    val zaehlwerke: List<Zaehlwerk>,
+    val zaehlerhersteller: Geschaeftspartner
+): Geschaeftsobjekt(boTyp = BOTyp.ZAEHLER)
