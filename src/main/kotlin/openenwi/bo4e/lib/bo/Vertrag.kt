ffad82748790ae4eadd9062c94662412f17cc51b
Florian Kubis <mail@kubisflo.de>
2021-08-02 09:16:11 +0200

    Add BO Vertrag
    refs #3

---
+++ src/main/kotlin/openenwi/bo4e/lib/bo/Vertrag.kt
@@ -0,0 +1,34 @@
+package openenwi.bo4e.lib.bo
+
+import openenwi.bo4e.lib.com.Unterschrift
+import openenwi.bo4e.lib.com.Vertragskonditionen
+import openenwi.bo4e.lib.com.Vertragsteil
+import openenwi.bo4e.lib.enum.BOTyp
+import openenwi.bo4e.lib.enum.Sparte
+import openenwi.bo4e.lib.enum.Vertragsart
+import openenwi.bo4e.lib.enum.Vertragsstatus
+import java.time.LocalDateTime
+
+/**
+ * Modell für die Abbildung von Vertragsbeziehungen.
+ * Das Objekt dient dazu, alle Arten von Verträgen, die in der Energiewirtschaft Verwendung finden, abzubilden.
+ *
+ * veröffentlicht am 05. September 2018
+ *
+ * @see "https://www.bo4e.de/dokumentation/geschaeftsobjekte/bo-vertrag"
+ */
+data class Vertrag(
+    val vertragsnummer: String,
+    val beschreibung: String?,
+    val vertragsart: Vertragsart,
+    val vertragstatus: Vertragsstatus,
+    val sparte: Sparte,
+    val vertragsbeginn: LocalDateTime,
+    val vertragsende: LocalDateTime,
+    val vertragspartner1: Geschaeftspartner,
+    val vertragspartner2: Geschaeftspartner,
+    val unterzeichnervp1: Unterschrift,
+    val unterzeichnervp2: Unterschrift,
+    val vertragskonditionen: Set<Vertragskonditionen>,
+    val vertragsteile: List<Vertragsteil>
+): Geschaeftsobjekt(boTyp = BOTyp.VERTRAG)
--- src/main/kotlin/openenwi/bo4e/lib/enum/BOTyp.kt
+++ src/main/kotlin/openenwi/bo4e/lib/enum/BOTyp.kt
@@ -32,5 +32,6 @@ enum class BOTyp {
     // Nicht im Standard:
     LASTGANG,
     LASTGANGKOMPAKT,
-    REGION
+    REGION,
+    VERTRAG
 }
