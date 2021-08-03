8b2e821fa4733af60c1df8ea1f3df1d32a53ae0e
Florian Kubis <mail@kubisflo.de>
2021-08-03 10:07:08 +0200

    Add BO Standorteigenschaften
    refs #3

---
+++ src/main/kotlin/openenwi/bo4e/lib/bo/Standorteigenschaften.kt
@@ -0,0 +1,19 @@
+package openenwi.bo4e.lib.bo
+
+import openenwi.bo4e.lib.com.StandorteigenschaftenAllgemein
+import openenwi.bo4e.lib.com.StandorteigenschaftenGas
+import openenwi.bo4e.lib.com.StandorteigenschaftenStrom
+import openenwi.bo4e.lib.enum.BOTyp
+
+/**
+ * In diesem BO werden die regionalen und spartenspezifischen Eigenschaften einer gegebenen Adresse modelliert.
+ *
+ * veröffentlicht am 03. Juli 2019
+ *
+ * @see "https://www.bo4e.de/dokumentation/geschaeftsobjekte/bo-standorteigenschaften"
+ */
+data class Standorteigenschaften(
+    val eigenschaftenAllgemein: StandorteigenschaftenAllgemein,
+    val eigenschaftenStrom: Set<StandorteigenschaftenStrom>,
+    val eigenschaftenGas: Set<StandorteigenschaftenGas>
+) : Geschaeftsobjekt(boTyp = BOTyp.STANDORTEIGENSCHAFTEN)
