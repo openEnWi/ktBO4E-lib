d6006532b2ad0490515c798fe688c6d5b3fb706e
Florian Kubis <mail@kubisflo.de>
2021-08-12 14:44:23 +0200

    Add BO Tarifinfo
    refs #3

---
+++ src/main/kotlin/openenwi/bo4e/lib/bo/Tarifinfo.kt
@@ -0,0 +1,29 @@
+package openenwi.bo4e.lib.bo
+
+import openenwi.bo4e.lib.com.Energiemix
+import openenwi.bo4e.lib.com.Vertragskonditionen
+import openenwi.bo4e.lib.com.Zeitraum
+import openenwi.bo4e.lib.enum.*
+
+/**
+ * Das BO Tarifinfo liefert die Merkmale, die einen Endkundentarif identifizierbar machen.
+ * Dieses BO dient als Basis für weitere BOs mit erweiterten Anwendungsmöglichkeiten.
+ *
+ * veröffentlicht am 30. Mai 2018
+ *
+ * @see "https://www.bo4e.de/dokumentation/geschaeftsobjekte/bo-tarifinfo"
+ */
+data class Tarifinfo(
+    val bezeichnung: String,
+    val anbietername: String,
+    val sparte: Sparte,
+    val kundentyp: Kundentyp,
+    val tarifart: Tarifart,
+    val tariftyp: Tariftyp,
+    val tarifmerkmal: Tarifmerkmal,
+    val website: String?,
+    val anbieter: Marktteilnehmer,
+    val zeitlicheGueltigkeit: Zeitraum?,
+    val energiemix: Energiemix?,
+    val vertragskonditionen: Vertragskonditionen?
+) : Geschaeftsobjekt(boTyp = BOTyp.TARIFINFO)
