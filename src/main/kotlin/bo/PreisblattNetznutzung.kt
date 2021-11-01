f1d87cae4de37a7da979b76273fcc5940a37e2ad
Florian Kubis <mail@kubisflo.de>
2021-11-01 12:00:40 +0100

    Add BO PreisblattNetznutzung

---
+++ src/main/kotlin/bo/PreisblattNetznutzung.kt
@@ -0,0 +1,44 @@
+package de.openenwi.bo4e.lib.bo
+
+import de.openenwi.bo4e.lib.com.Preisposition
+import de.openenwi.bo4e.lib.com.Zeitraum
+import de.openenwi.bo4e.lib.enum.*
+
+/**
+ * Die Variante des Preisblattmodells zur Abbildung der Netznutzungspreise.
+ *
+ * veröffentlicht am 11. April 2018
+ *
+ * @see [BO PreisblattKonzessionsabgabe](https://www.bo4e.de/dokumentation/geschaeftsobjekte/bo-preisblattkonzessionsabgabe)
+ */
+data class PreisblattNetznutzung(
+    val bilanzierungsmethode: Bilanzierungsmethode,
+    val netzebene: Netzebene,
+    val kundengruppe: Kundengruppe,
+    private val preisblatt: Preisblatt,
+) : Preisblatt by preisblatt {
+    constructor(
+        bilanzierungsmethode: Bilanzierungsmethode,
+        netzebene: Netzebene,
+        kundengruppe: Kundengruppe,
+
+        bezeichnung: String,
+        sparte: Sparte,
+        preisstatus: Preisstatus,
+        herausgeber: Marktteilnehmer?,
+        gueltigkeit: Zeitraum,
+        preispositionen: Set<Preisposition>,
+    ) : this(
+        bilanzierungsmethode,
+        netzebene,
+        kundengruppe,
+        PreisblattImpl(
+            bezeichnung,
+            sparte,
+            preisstatus,
+            herausgeber,
+            gueltigkeit,
+            preispositionen,
+        )
+    )
+}
