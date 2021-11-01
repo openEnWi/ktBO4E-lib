53a689299ef9b68223cf7f45dc7377e513fb0f87
Florian Kubis <mail@kubisflo.de>
2021-11-01 11:50:46 +0100

    Add BO PreisblattHardware Add docblock to BO PreisblattDienstleistung

--- src/main/kotlin/bo/PreisblattHardware.kt
+++ src/main/kotlin/bo/PreisblattHardware.kt
@@ -3,30 +3,29 @@ package de.openenwi.bo4e.lib.bo
 import de.openenwi.bo4e.lib.com.Geraeteeigenschaften
 import de.openenwi.bo4e.lib.com.Preisposition
 import de.openenwi.bo4e.lib.com.Zeitraum
-import de.openenwi.bo4e.lib.enum.Bilanzierungsmethode
-import de.openenwi.bo4e.lib.enum.Dienstleistungstyp
-import de.openenwi.bo4e.lib.enum.Preisstatus
-import de.openenwi.bo4e.lib.enum.Sparte
+import de.openenwi.bo4e.lib.enum.*
 
 /**
- * Die Variante des Preisblattmodells zur Abbildung der Preise für wahlfreie Dienstleistungen.
+ * Die Variante des Preisblattmodells zur Abbildung der Preise für zusätzliche Hardware.
  *
- * veröffentlicht am 25. April 2018
+ * veröffentlicht am 11. Dezember 2019
  *
- * @see [BO PreisblattDienstleistung](https://www.bo4e.de/dokumentation/geschaeftsobjekte/bo-preisblattdienstleistung)
+ * @see [BO PreisblattHardware](https://www.bo4e.de/dokumentation/geschaeftsobjekte/preisblatthardware)
  */
-data class PreisblattDienstleistung(
+data class PreisblattHardware(
     val bilanzierungsmethode: Bilanzierungsmethode,
-    val basisdienstleistung: Dienstleistungstyp,
+    val messebene: Netzebene,
     val inklusivedienstleistung: Set<Dienstleistungstyp>,
-    val geraetedetails: Geraeteeigenschaften?,
+    val basisgeraet: Geraeteeigenschaften?,
+    val inklusivegeraete: Set<Geraeteeigenschaften>,
     private val preisblatt: Preisblatt,
 ) : Preisblatt by preisblatt {
     constructor(
         bilanzierungsmethode: Bilanzierungsmethode,
-        basisdienstleistung: Dienstleistungstyp,
+        messebene: Netzebene,
         inklusivedienstleistung: Set<Dienstleistungstyp>,
-        geraetedetails: Geraeteeigenschaften?,
+        basisgeraet: Geraeteeigenschaften?,
+        inklusivegeraete: Set<Geraeteeigenschaften>,
         bezeichnung: String,
         sparte: Sparte,
         preisstatus: Preisstatus,
@@ -35,9 +34,10 @@ data class PreisblattDienstleistung(
         preispositionen: Set<Preisposition>,
     ) : this(
         bilanzierungsmethode,
-        basisdienstleistung,
+        messebene,
         inklusivedienstleistung,
-        geraetedetails,
+        basisgeraet,
+        inklusivegeraete,
         PreisblattImpl(
             bezeichnung,
             sparte,
